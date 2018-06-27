0b4164c5e63c0090f17f41127188e2c4ad6360ee
Alice Jacka <harry@jacka.net.au>
2018-06-27 21:21:10 +1000

    add sql manager

---
+++ eTimesheets/includes/sql.php
@@ -0,0 +1,10 @@
+<?php
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
