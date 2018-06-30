a647c3cb541d2692d57457f022ad0fbf6775196f
Alice Jacka <harry@jacka.net.au>
2018-06-30 11:32:54 +1000

    added getEmployeeList function

--- eTimesheets/includes/sql.php
+++ eTimesheets/includes/sql.php
@@ -4,7 +4,26 @@
 $dbc = new mysqli($config['sql']['addr'], $config['sql']['uname'], $config['sql']['passwd'], $config['sql']['db']);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($dbc->connect_error) {
+	die('<span style="color: red;">Connection to SQL server failed: ' . $dbc->connect_error . '</span>');
 }
-echo "Connected successfully";
+
+function getEmployeeList() // get a list of employees usernames
+{
+    global $dbc; // get access to the dbc
+
+    $stmt = $dbc->prepare('SELECT * FROM employees'); // prepare a request
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    if($result->num_rows === 0) return false; // if no matchs were found, something's gome wrong so ret0
+
+    $stmt->close();
+
+    foreach ($result->fetch_all() as $user) { // loop through each user
+        $ret[] = $user[1]; // extract the uname to a return array
+    }
+
+    return $ret; // return the list of unames
+}
+
