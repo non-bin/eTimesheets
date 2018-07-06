9569bf584d51da13a1375ea3c26c9dff2e8e4804
Alice Jacka <harry@jacka.net.au>
2018-07-06 17:00:07 +1000

    employeeClass.php: added getEvents method

--- eTimesheets/includes/employeeClass.php
+++ eTimesheets/includes/employeeClass.php
@@ -37,4 +37,32 @@ class Employee
 			return false;
 		}
 	}
+
+	public function getEvents($dateBegin = null, $dateEnd = null)
+	{
+		global $dbc; // get access to the dbc
+
+		if ($dateBegin == null) {
+			$dateBegin = '0000-00-00 00:00:00';
+		}
+
+		if ($dateEnd == null) {
+			$dateEnd   = '9999-12-31 23:59:59';
+		}
+
+		$stmt = $dbc->prepare('SELECT * FROM timesheet WHERE uid = ? AND datetime between ? AND ? order BY datetime DESC;'); // prepare a request
+		$stmt->bind_param('iss', $this->uid, $dateBegin, $dateEnd);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+		if($result->num_rows === 0) return false; // if no matchs were found, something's gome wrong so ret0
+
+		$stmt->close();
+
+		foreach ($result->fetch_all() as $row) { // loop through each row
+			$ret[] = new Event($row[0], $row[1], $row[2], $row[3]);
+		}
+
+		return $ret;
+	}
 }
