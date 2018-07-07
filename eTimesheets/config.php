6992200a3349f0fa3d743d22ddbeb6f08d729251
Alice Jacka <harry@jacka.net.au>
2018-07-07 12:36:08 +1000

    config.php: added title config entry

--- eTimesheets/config.php
+++ eTimesheets/config.php
@@ -1,9 +1,11 @@
 <?php
 
+$config['main']['title']   = 'eTimesheets'; // custon title (eg page titles, headings etc)
+
 // MySQL database
-$config['sql']['addr']   = 'localhost'; // server address
+$config['sql']['addr']   = 'localhost';   // server address
 $config['sql']['uname']  = "application"; // username and
 $config['sql']['passwd'] = "klNIdhU(75^"; // password of user
-$config['sql']['db']   = 'eTimesheets'; // datamase name
+$config['sql']['db']     = 'eTimesheets'; // datamase name
 
 ?>
