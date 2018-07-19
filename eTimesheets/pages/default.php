6b53404ea5adca13736be7ceed7e6e35fd59d04b
Alice Jacka <harry@jacka.net.au>
2018-07-19 19:33:45 +1000

    pages/default.php: updates I guess

--- eTimesheets/pages/default.php
+++ eTimesheets/pages/default.php
@@ -1,14 +1,21 @@
 <?php
 
-require '../includes/pageDefault.php';
+session_start();
 
-if (isset($_GET['uid'])) {
-    $uid = $_GET['uid'];
-} else {
-    $uid = 1;
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 120)) {
+    // last request was more than 2 minutes ago
+    session_unset();   // unset $_SESSION variable for the run-time
+    session_destroy(); // destroy session data in storage
 }
+$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
 
-$output['title'] = $config['main']['title'] . ' - home'; // define title
+require '../includes/pageDefault.php'; // this file contains all functions needed in the default page
+
+$uid    = (isset($_GET['uid'])) ? $_GET['uid'] : 1 ;               // if a uid was specified use it, if not, use 1
+$action = (isset($_GET['action'])) ? $_GET['action'] : 'default' ; // if an action was specified use it, if not, use 'default'
+
+$output['title'] = $config['main']['title'] . ' - home'; // define the title using the configured prefix
+$output['uid']   = $uid;
 
 $output['style']  = '';
 $output['header'] = '';
@@ -16,17 +23,46 @@ $output['main']   = '';
 $output['footer'] = '';
 $output['action'] = '';
 
+// calculate the background colour
+$backHsl    = hexToHsl('ffc6c6');
+$backHsl[0] = calcHueRotate($uid)/360; // rotate the hue to the user's unique one
+$background = hslToHex($backHsl);
 
-$tmp = hexToHsl('ffc6c6');
-$tmp[0] = calcHueRotate($uid)/360;
-$output['style'] .= 'body { background-color: #' . hslToHex($tmp) . '; }';
+// do the same for the foreground
+$foreHsl    = hexToHsl('ff5d55');
+$foreHsl[0] = calcHueRotate($uid)/360;
+$foreground = hslToHex($foreHsl);
 
+// use the generated colours in a style
+$output['style'] .= 'body { background-color: #' . $background . '; } .foreground { background-color: #' . $foreground . '; }';
+
+// generate the user selection aria
 $empList = getEmployeeList();
 foreach ($empList as $emp) {
     $output['header'] .= '
     <a class="avatar" href="?p=default&uid= ' . $emp[0] . '">
         <img class="avatar" style="filter: hue-rotate(' . calcHueRotate($emp[0]) . 'deg)" src="./img/avatar.png" alt="username">
     </a>';
+}
+
+// generate the main action button
+switch ($action) {
+    case 'si':
+        $output['actionContent'] = '
+        <a href="?p=default&uid=' . $output['uid'] . '&act=' . $output['action'] . '">
+        </a>';
+        break;
+
+    default:
+        $output['actionContent'] = '
+        <span class="action foreground">
+            <span class="action-lable">
+                <form action="?p=default&uid=' . $output['uid'] . '&act=login" method="post">
+                    <input type="text" name="pin" id="pin-input" placeholder="Enter Pin">
+                </form>
+            </span>
+        </span>';
+        break;
 }
 
 ?>
@@ -38,7 +74,7 @@ foreach ($empList as $emp) {
     <title><?=$output['title'] ?></title>
     <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="./css/default.css">
-    <?='<style>' . $output['style'] . '</style>' ?>
+    <?='<style>' . $output['style'] . '</style>' // for some reason having the style tags outside of php messed with the formating ?>
 </head>
 <body>
 
@@ -47,10 +83,7 @@ foreach ($empList as $emp) {
 </span>
 
 <span class="main">
-    <a class="avatar" href="?p=default&uid=<?=$uid ?>&act=<?=$output['action'] ?>">
-        <span class="action">
-            <span class="action-lable">Log In</span>
-        </span>
+        <?=$output['actionContent'] ?>
 </span>
 
 <span class="footer">
