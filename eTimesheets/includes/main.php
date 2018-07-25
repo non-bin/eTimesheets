0cedb7b3eb134d75487af8f700e906cf9dc94621
Alice Jacka <harry@jacka.net.au>
2018-07-25 20:39:58 +1000

    main.php: moved getEmployeeList from sql.php

--- eTimesheets/includes/main.php
+++ eTimesheets/includes/main.php
@@ -1,7 +1,33 @@
 <?php
 
+/**
+ * Harry Jacka main.php 1.0 (created 25/7/18)
+ *
+ * contains functions and methods commonly used in
+ * multiple main files
+ */
+
 function destroySession()
 {
     session_unset();   // unset $_SESSION variable for the run-time
     session_destroy(); // destroy session data in storage
+}
+
+function getEmployeeList() // get a list of employee's uids and unames
+{
+    global $dbc; // get access to the dbc
+
+    $stmt = $dbc->prepare('SELECT * FROM `employees`'); // prepare a request
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    if($result->num_rows === 0) return false; // if no matchs were found, something's gome wrong so ret0
+
+    $stmt->close();
+
+    foreach ($result->fetch_all() as $user) { // loop through each user
+        $ret[] = [$user[0], $user[1]]; // extract the uid and uname to a return array
+    }
+
+    return $ret; // return the list of uids and unames
 }
--- eTimesheets/includes/sql.php
+++ eTimesheets/includes/sql.php
@@ -7,22 +7,3 @@ $dbc = new mysqli($config['sql']['addr'], $config['sql']['uname'], $config['sql'
 if ($dbc->connect_error) {
     die('<span style="color: red;">Connection to SQL server failed: ' . $dbc->connect_error . '</span>');
 }
-
-function getEmployeeList() // get a list of employee's uids and unames
-{
-    global $dbc; // get access to the dbc
-
-    $stmt = $dbc->prepare('SELECT * FROM `employees`'); // prepare a request
-    $stmt->execute();
-
-    $result = $stmt->get_result();
-    if($result->num_rows === 0) return false; // if no matchs were found, something's gome wrong so ret0
-
-    $stmt->close();
-
-    foreach ($result->fetch_all() as $user) { // loop through each user
-        $ret[] = [$user[0], $user[1]]; // extract the uid and uname to a return array
-    }
-
-    return $ret; // return the list of uids and unames
-}
