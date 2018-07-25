ed48e0836743c1e960be26788477ff3f8187632d
Alice Jacka <harry@jacka.net.au>
2018-07-25 19:14:44 +1000

    admin.php: added main admin manager

--- eTimesheets/pages/admin.php
+++ eTimesheets/pages/admin.php
@@ -1,3 +1,48 @@
 <?php
 
-echo 'admin-login';
+/// session ///
+
+session_start();
+
+
+/// GET arguments ///
+
+$adminPage = (isset($_GET['a'])) ? $_GET['a'] : 'login' ; // if a page was requested
+
+
+/// login handler ///
+
+if ($action == 'auth') { // authenticate a logon request
+    # code...
+}
+
+
+/// authentication ///
+
+// this is seperate from the login handeler
+// because a user stays logged on, so actions
+// can be performed without relogging in
+
+
+
+
+/// admin page selection ///
+
+switch ($adminPage) {
+    case 'home': // home page
+        # code...
+        break;
+
+    case 'individual': // individual summary page
+        # code...
+        break;
+
+    case 'amend': // information amment page
+        # code...
+        break;
+
+    case 'login': // display the login screen
+    default:
+        # code...
+        break;
+}
