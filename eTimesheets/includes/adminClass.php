f311952cae44d8357e860cf80514a78e30cd7008
Alice Jacka <harry@jacka.net.au>
2018-07-26 18:38:27 +1000

    adminClass.php: created the admin class

--- eTimesheets/includes/adminClass.php
+++ eTimesheets/includes/adminClass.php
@@ -1,27 +1,41 @@
 <?php
 
 /**
- * Harry Jacka adminClass.php 0.1 (created 5/7/18)
+ * Harry Jacka adminClass.php 0.2 (created 5/7/18)
  *
  * handle every action an admin may perform
  */
 
-die('MAKE THE ADMIN A CLASS!');
-
-function getAdminUser(String $uname)
+class Admin
 {
-    global $dbc; // get access to the dbc
+    private $passwdHash; // the password hash should only be accessable to functions inside the class
 
-    $stmt = $dbc->prepare('SELECT * FROM `admin_users` WHERE `uname` = ?'); // prepare a request
-    $stmt->bind_param('s', $uname); // bind the uname input to the statment
-    $stmt->execute();
+    public $uname;
 
-    $result = $stmt->get_result();
-    if ($result->num_rows !== 1) {
-        return false; // if 0, or >1 matchs were found, something's gome wrong so ret0
+    public function __construct(string $uname) {
+        global $dbc; // get access to the dbc
+
+        $stmt = $dbc->prepare('SELECT * FROM `admin_users` WHERE `uname` = ?'); // prepare a request
+        $stmt->bind_param('s', $uname); // bind the uname input to the statment
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        if ($result->num_rows !== 1) {
+            return false; // if 0, or >1 matchs were found, something's gome wrong so ret0
+        }
+
+        $stmt->close();
+
+        $this->passwdHash  = $result->fetch_assoc()['passwd'];
+        $this->uname       = $uname;
     }
 
-    $stmt->close();
+    public function checkPassword(string $password) // check the password against the hash
+    {
+        if (password_verify($password, $this->passwdHash)) {
+            return true;
+        }
 
-    return $result->fetch_assoc(); // return the row
+        return false;
+    }
 }
