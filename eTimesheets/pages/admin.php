5f1bc3b6664d1406ab4d6c952cadd89ab0b5ebb0
Alice Jacka <harry@jacka.net.au>
2018-07-28 14:51:38 +1000

    admin.php: remove signup restriction

--- eTimesheets/pages/admin.php
+++ eTimesheets/pages/admin.php
@@ -49,16 +49,14 @@ if ($action == 'auth') { // authenticate a logon request
 if ($action == 'su') { // process a sign up request
     if (isset($_POST['uname'], $_POST['password'], $_POST['confirm']) && $_POST['uname'] != '' && $_POST['password'] != '' && $_POST['confirm'] != '') { // check all values recieved
         if ($_POST['password'] === $_POST['confirm']) {                  // check passwords identical
-            $output['error'] = 'sign up failed: it\'s not going to be that easy sorry :) I thought I should disable this';
-            error_log($output['error']);
-            $action = 'signUp';
-            // if (addAdminUser($_POST['uname'], $_POST['password'])) {     // add the user
-            //     $action = 'login';
-            //     error_log('sign up sucess: for uname "' . $_POST['uname'] . '"');
-            // } else {
-            //     $output['error'] = 'sign up failed: database error'; // save an error to be reported to the user
-            //     error_log($output['error']);                         // and log it to console
-            // }
+            if (addAdminUser($_POST['uname'], $_POST['password'])) {     // add the user
+                error_log('sign up sucess: for uname "' . $_POST['uname'] . '"');
+                $action = 'login';
+            } else {
+                $output['error'] = 'sign up failed: database error'; // save an error to be reported to the user
+                error_log($output['error']);                         // and log it to console
+                $action = 'signUp';
+            }
         } else {
             $output['error'] = 'sign up failed: passwords do not match';
             error_log($output['error']);
