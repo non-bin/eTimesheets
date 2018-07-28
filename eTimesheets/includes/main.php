ffe749e48778b153b44cecbe75fab338c1859fcb
Alice Jacka <harry@jacka.net.au>
2018-07-28 15:05:25 +1000

    main.php: actually fixed the uninitialised session problem now

--- eTimesheets/includes/main.php
+++ eTimesheets/includes/main.php
@@ -9,9 +9,7 @@
 
 function destroySession()
 {
-    $_SESSION['null'] = true; // avoid errors regarding uninitialised session
-    session_unset();   // unset $_SESSION variable for the run-time
-    session_destroy(); // destroy session data in storage
+    session_unset();   // destroy the contents of $_SESSION
 }
 
 function getEmployeeList() // get a list of employee's uids and unames
